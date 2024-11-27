c5cdaba463effa7f18765fba9ff4b6293ba77806
rhys-jones-20 <rhys.jones1@cabinetoffice.gov.uk>
2024-11-27 11:53:42 +0000

    Added topic database tables

---
+++ scout/TopicModelling/storage/postgres_models.py
@@ -0,0 +1,54 @@
+import uuid
+
+from sqlalchemy import Column, DateTime, Float, ForeignKey, Integer, String, func
+from sqlalchemy.dialects.postgresql import UUID, ARRAY
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+TopicBase = declarative_base()
+
+class TopicModelSpec(TopicBase):
+    __tablename__ = "topic_model"
+    __table_args__ = {"schema": "topic_modelling"}
+
+    name = Column(String)
+    id = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
+    num_docs = Column(Integer)
+    computation_time = Column(Float)
+    num_topics = Column(Integer)
+    created_datetime = Column(DateTime(timezone=True), server_default=func.now())
+    updated_datetime = Column(DateTime(timezone=True), onupdate=func.now())
+
+    topics = relationship("Topic", back_populates="topic_model")
+
+
+class Topic(TopicBase):
+    __tablename__ = "topics"
+    __table_args__ = {"schema": "topic_modelling"}
+
+    id = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
+    topic_num = Column(Integer)
+    representation = Column(String)
+    summary = Column(String)
+    primary_theme = Column(String)
+    top_words = Column(ARRAY(String))
+    num_documents = Column(Integer)
+    created_datetime = Column(DateTime(timezone=True), server_default=func.now())
+    updated_datetime = Column(DateTime(timezone=True), onupdate=func.now())
+
+    topic_model_id = Column(UUID, ForeignKey("topic_modelling.topic_model.id"))
+    topic_model = relationship("TopicModelSpec", back_populates="topics")
+
+    chunk_associations = relationship("ChunkTopicJoin", back_populates="topic")
+
+class ChunkTopicJoin(TopicBase):
+    __tablename__ = 'chunk_topic_associations'
+    __table_args__ = {'schema': 'topic_modelling'}
+
+    id = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
+    chunk_id = Column(UUID, nullable=False)  # Reference to base application's chunk
+    topic_id = Column(UUID, ForeignKey('topic_modelling.topics.id'))
+    relevance_score = Column(Float, nullable=False)
+
+    topic = relationship("Topic", back_populates="chunk_associations")
+
